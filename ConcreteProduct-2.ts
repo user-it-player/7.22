e497522373d32c065758ed4f0833f47294f3fe14
user-it-player <66707889+user-it-player@users.noreply.github.com>
2020-07-22 10:52:26 +0900

    Add files via upload

---
+++ ConcreteProduct-2.ts
@@ -0,0 +1,10 @@
+class Radio extends Seihin {
+    private int radioSerialNumber;
+    public void numberring() {
+    radioSerialNumber = Counter.getRadioNumber();
+}
+public void print() {
+    System.out.println("このラジオに関する情報:");
+    System.out.println(" 製造番号:" + radioSerialNumber);
+}
+}
---
+++ Creator.ts
@@ -0,0 +1,9 @@
+abstract class Koujyou {
+    public final Seihin create() {
+        Seihin seihin = factoryMethod(); touroku(seihin);
+        return seihin;
+    }
+    public abstract Seihin factoryMethod();
+
+    public abstract void touroku(Seihin s);
+}
